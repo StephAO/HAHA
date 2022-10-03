294934e295fe0ec0be5e7f4e119bb8283a7c84f5
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2022-10-03 16:05:02 -0600

    fix get_arguments not being defined in base agent issue (used in load_agent)

--- oai_agents/agents/base_agent.py
+++ oai_agents/agents/base_agent.py
@@ -1,5 +1,5 @@
 from oai_agents.common.state_encodings import ENCODING_SCHEMES
-from oai_agents.common.arguments import get_args_to_save, set_args_from_load
+from oai_agents.common.arguments import get_args_to_save, set_args_from_load, get_arguments
 
 from overcooked_ai_py.mdp.overcooked_mdp import Action
 
