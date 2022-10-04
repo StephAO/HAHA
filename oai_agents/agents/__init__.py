2f9090bd95bf342a87bbfc43f800d85ff4b77b18
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2022-10-04 07:42:52 -0600

    add obs required for hrl in action

--- oai_agents/agents/__init__.py
+++ oai_agents/agents/__init__.py
@@ -6,9 +6,41 @@ from oai_agents.common.arguments import get_arguments
 from pathlib import Path
 import torch as th
 
+def fix_save_pickle(agent_path, args=None):
+    import oai_agents
+    args = args or get_arguments()
+    agent_path = Path(agent_path)
+
+    try:
+        load_dict = th.load(agent_path / 'agent_file')
+        print(load_dict)
+    except FileNotFoundError as e:
+        raise ValueError(f'Could not find file:{e}') # TODO print options
+
+    save_dict = {}
+    for k, v in load_dict.items():
+        # print(k, v)
+        # print('__main__' in str(v))
+        if '__main__.MultiAgentSubtaskWorker' in str(v):
+            v = oai_agents.agents.MultiAgentSubtaskWorker
+        elif '__main__.HierarchicalRL' in str(v):
+            v = oai_agents.agents.HierarchicalRL
+        elif '__main__' in str(v):
+            print(v)
+            # cls = oai_agents.agents.HierarchicalRL
+            # save_dict[k.replace('__main__', 'oai_agents.agents')] = v
+        save_dict[k] = v
+    print(load_dict)
+    print(save_dict)
+    th.save(save_dict, agent_path / 'agent_file_fixed')
+
+
+
 if __name__ == '__main__':
     from oai_agents.common.arguments import get_arguments
-    args = get_arguments()
-    agent = load_agent(Path('test_data'), args)
-    print(type(agent))
+    fix_save_pickle('/home/miguel/Documents/projects/overcooked-demo/server/static/assets/agents/oai_hrl/manager')
+
+    # args = get_arguments()
+    # agent = load_agent(Path('test_data'), args)
+    # print(type(agent))
 
