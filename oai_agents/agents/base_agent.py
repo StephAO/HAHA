4830bb6385635037db79d77adb3aeeeed25c71a5
Stephane Aroca-Ouellette <stephanearocaouellette@gmail.com>
2022-10-12 14:23:27 -0600

    hopefully last fix

--- oai_agents/agents/base_agent.py
+++ oai_agents/agents/base_agent.py
@@ -87,14 +87,15 @@ class OAIAgent(nn.Module, ABC):
                 get_doable_subtasks(state, self.prev_st, self.terrain, self.p_idx, USEABLE_COUNTERS - 1).astype(bool)
         if 'player_completed_subtasks' in self.policy.observation_space.keys():
             # If this isn't the first step of the game, see if a subtask has been completed
-            comp_st = [calculate_completed_subtask(self.terrain, self.prev_state, state, i) for i in range(2)]
-            # If a subtask has been completed, update counts
-            if comp_st[p_idx] is not None:
-                self.player_completed_tasks[comp_st[p_idx]] += 1
-                self.prev_st = comp_st[p_idx]
-            if comp_st[1 - p_idx] is not None:
-                self.player_completed_tasks[comp_st[1 - p_idx]] += 1
-            # If this is the first step of the game, reset subtask counts to 0
+            if self.prev_state is not None:
+                comp_st = [calculate_completed_subtask(self.terrain, self.prev_state, state, i) for i in range(2)]
+                # If a subtask has been completed, update counts
+                if comp_st[p_idx] is not None:
+                    self.player_completed_tasks[comp_st[p_idx]] += 1
+                    self.prev_st = comp_st[p_idx]
+                if comp_st[1 - p_idx] is not None:
+                    self.player_completed_tasks[comp_st[1 - p_idx]] += 1
+                # If this is the first step of the game, reset subtask counts to 0
             else:
                 self.player_completed_tasks = np.zeros(Subtasks.NUM_SUBTASKS)
                 self.tm_completed_tasks = np.zeros(Subtasks.NUM_SUBTASKS)
